b3a374c73131d02913e8ff79d12e2527f45dffda
huulinh99 <43189610+huulinh99@users.noreply.github.com>
2022-01-08 09:24:05 +0700

    Commit for first code in project

---
+++ Persistence/Migrations/20220107030420_seed-data.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Persistence.Migrations
+{
+    public partial class seeddata : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Activties",
+                table: "Activties");
+
+            migrationBuilder.RenameTable(
+                name: "Activties",
+                newName: "Activities");
+
+            migrationBuilder.RenameColumn(
+                name: "Veune",
+                table: "Activities",
+                newName: "Venue");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Activities",
+                table: "Activities",
+                column: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Activities",
+                table: "Activities");
+
+            migrationBuilder.RenameTable(
+                name: "Activities",
+                newName: "Activties");
+
+            migrationBuilder.RenameColumn(
+                name: "Venue",
+                table: "Activties",
+                newName: "Veune");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Activties",
+                table: "Activties",
+                column: "Id");
+        }
+    }
+}
